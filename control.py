b59084b02f238a49fa2c6940a1ac7402e5e58e30
Justus Pendleton <justus@ryoohki.net>
2020-05-12 16:20:58 +0700

    allow relative and absolute control lines

--- control.py
+++ control.py
@@ -44,7 +44,8 @@ def parse_args():
                         help="Minimum date")
     parser.add_argument('--ignore-account', action='append', default=[])
     parser.add_argument('--currency', help="Currency to report in")
-    parser.add_argument('--control-limit', nargs=2, action='append', help="Define a new control limit with a name and a value. e.g. --control-limit danger 0.05")
+    parser.add_argument('--control-limit', nargs=2, action='append', help="Define a new control limit with a name and a percentage value. e.g. --control-limit danger 0.05")
+    parser.add_argument('--control-limit-absolute', nargs=2, action='append', help="Define a new control limit with a name and an absolute value.")
     parser.add_argument('--output', default='process-chart.png', help="The filename to write the graph to")
     parser.add_argument('--display', default=False, action='store_true', help='Display the chart instead of saving it to a file')
     parser.add_argument('--rolling', default=12, type=int, help='How many months to use when calculating the rolling average.')
@@ -137,14 +138,17 @@ def build_dataframe(args):
     df = pandas.concat([spending_rate, moving_average], axis=1, sort=False)
     df.columns = ['monthly spending rate', 'moving average']
 
+    # define all of the control limits.
+    for (name, value) in args.control_limit:
+        df[name] = float(value)
+
+    for (name, value) in args.control_limit_absolute:
+        df[name] = float(value) / networth_series
+
     return df
 
 def chart(df, args):
     df['date'] = df.index
-
-    # define all of the control limits.
-    for (name, value) in args.control_limit:
-        df[name] = float(value)
 
     melted = df.melt(id_vars='date')
     stdev = df['monthly spending rate'].std()
@@ -157,9 +161,10 @@ def chart(df, args):
     
     seaborn.despine(ax=ax, left=True, bottom=True, offset=20) 
     plt.xticks(rotation=45)
-    # add in stdevs, leave it 1 stdev for now but consider using 2stdevs in future
+    # add in stdevs, leave it 1 stdev for now but consider using 2stdevs in future.
+    # we really only care about upward variance (i.e. more spending that expected)
     plt.fill_between(df.index,
-            df['moving average'] - stdev,
+            df['moving average'],
             df['moving average'] + stdev,
             alpha=0.2)
     ax.set_ylim(bottom=0)
